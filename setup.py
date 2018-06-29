e5548de73d9acdfb46a60e10af7ec828bbead6c7
Maryam Ghanizadeh <maryamghanizadeh@Maryams-MBP-2.attlocal.net>
2018-06-29 00:10:15 -0500

    Added setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+description = (
+    'A Python Codes for practicing.')
+
+setup(
+    name='codingChallenges',
+    version='0.0.1',
+    description=description,
+    long_description=description,
+    author='Maryam Ghanizadeh',
+    author_email='maloochpich@gmail.com',
+     packages=find_packages()
+)
