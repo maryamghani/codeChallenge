31c27902aa8979c4af14c9089b8a945e4583109e
Maryam Ghanizadeh <maryamghanizadeh@Maryams-MBP-2.attlocal.net>
2018-06-28 22:21:56 -0500

    bubble sort array

--- bubble_sort.py
+++ bubble_sort.py
@@ -11,16 +11,16 @@ def bubble_sort(array):
 def test_bubble_sort():
     array1 = [5, 1, 4, 2, 8]
     sorted_array = bubble_sort(array1)
-    assert sorted_array == array1
+    assert sorted_array == sorted(array1)
 
 
 def test_bubble_sort_with_replica():
     array1 = [5, 1, 1, 2, 8]
     sorted_array = bubble_sort(array1)
-    assert sorted_array == array1
+    assert sorted_array == sorted(array1)
 
 
 def test_bubble_sort_with_all_same_input():
     array1 = [1, 1, 1]
     sorted_array = bubble_sort(array1)
-    assert sorted_array == array1
+    assert sorted_array == sorted(array1)
