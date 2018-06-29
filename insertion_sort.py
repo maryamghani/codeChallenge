1efcca535420d6c972a43cb112297d018982cffe
Maryam Ghanizadeh <maryamghanizadeh@Maryams-MBP-2.attlocal.net>
2018-06-28 23:33:23 -0500

    add insertion sort

---
+++ insertion_sort.py
@@ -0,0 +1,27 @@
+"""
+Insertion Sort array
+"""
+
+
+def insertion_sort_first_approach(array):
+    for i in range(1, len(array)):
+        for j in range(i):
+            if array[i] < array[j]:
+                current = array[i]
+                array[i] = array[j]
+                array[j] = current
+
+
+def insertion_sort_second_approach(array):
+    for i in range(1, len(array)):
+        current_index = array[i]
+        j = i - 1
+        while j >= 0 and current_index < array[j]:
+            array[j+1] = array[j]
+            j -= 1
+        array[j + 1] = current_index
+
+
+if __name__ == '__main__':
+    input_test = [12, 11, 13, 5, 6]
+    insertion_sort_second_approach(input_test)
